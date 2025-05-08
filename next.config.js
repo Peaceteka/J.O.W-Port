52568c85c8841d6ccaf9ee9f19502829c3d1aab2
Peaceteka <peaceteka2@gmail.com>
2025-05-08 22:18:32 +0300

    Switch to web service deployment

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
   },
 }
 
